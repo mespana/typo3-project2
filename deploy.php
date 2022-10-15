95615d1c825fa1e58ef445c0158b0e8fb6007a04
mespana <marianaespana@yahoo.es>
2022-10-15 18:21:39 +0200

    first commit

---
+++ deploy.php
@@ -0,0 +1,77 @@
+<?php
+namespace Deployer;
+
+require 'recipe/typo3.php';
+
+// Project name
+set('application', 'typo3-project2');
+
+// Project repository
+set('repository', 'git@github.com:mespana/typo3-project2.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false); 
+
+// Set maximum releases backup
+set('keep_releases', 5);
+
+// To solve this issue: Cant't detect http user name. Please set up the 'http_user' config parameter.
+set('http_user', 'www-data');
+set('writable_mode', 'chmod');
+set('use_relative_symlink', '0');
+
+// Set Server
+//inventory('.hosts.yaml');
+
+// Hosts
+host('ftp109730-2622751@marianaespana.com')
+->set('deploy_path', '~/www/marianaespana/proyectos/typo3-project2');
+
+//DocumentRoot / WebRoot for the TYPO3 automaticInstallation
+set('typo3_webroot', 'public');
+
+// Shared directories
+set('shared_dirs', [
+    '{{typo3_webroot}}/fileadmin',
+    '{{typo3_webroot}}/typo3temp',
+    '{{typo3_webroot}}/uploads'
+]);
+
+// Shared files
+set('shared_files', [
+    '{{typo3_webroot}}/.htaccess'
+]);
+
+// Writable dirs by web server
+set('writable_dirs', [
+    'config',
+    'var',
+    '{{typo3_webroot}}/fileadmin',
+    '{{typo3_webroot}}/typo3temp',
+    '{{typo3_webroot}}/typo3conf',
+    '{{typo3_webroot}}/uploads'
+]);
+
+// Main TYPO3 Tasks
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy your project');
+after('deploy', 'success');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+
+
+
+
---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,123 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$LjNmL3NkOWhMWkk3MG94Tg$0TfPQ2JFyhqumkVam2Q2PBcNiIfIOaoucUWxfNqwgBQ',
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '943fc44ac3d62d177cc916d4985a93cd9b08c8bcea7b24fc9c42672914ce548d527310bc01111d462557b86c1208bec2',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'felogin.extbase' => true,
+            'fluidBasedPageModule' => true,
+            'rearrangedRedirectMiddlewares' => true,
+            'unifiedPageTranslationHandling' => true,
+            'yamlImportsFollowDeclarationOrder' => true,
+        ],
+        'sitename' => 'Wittemaier Bau GmbH',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
